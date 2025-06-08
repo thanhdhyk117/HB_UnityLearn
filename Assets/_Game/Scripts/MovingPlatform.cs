bf6c345738cd39412a418616f7454fdc2602dfc5
thanhnt0201 <thanhnt02012000@gmail.com>
2025-06-08 09:00:57 +0700

    ~Use couroutine to move

--- Assets/_Game/Scripts/MovingPlatform.cs
+++ Assets/_Game/Scripts/MovingPlatform.cs
@@ -9,30 +9,21 @@ public class MovingPlatform : MonoBehaviour
 
     [SerializeField] private Vector3 targetPosition;
     [SerializeField] private float speed = 2f;
+
+    [SerializeField] bool isEnd = false;
     // Start is called before the first frame update
     void Start()
     {
         transform.position = pointA.position; // Bắt đầu tại điểm A
         targetPosition = pointB.position; // Mục tiêu là điểm B
+
+        StartCoroutine(MovePointToPoint());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
-        {
-            // Đổi hướng di chuyển khi đến điểm B
-            if (targetPosition == pointB.position)
-            {
-                targetPosition = pointA.position; // Chuyển sang điểm A
-            }
-            else
-            {
-                targetPosition = pointB.position; // Chuyển sang điểm B
-            }
-        }
+        Move();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -51,4 +42,52 @@ public class MovingPlatform : MonoBehaviour
             collision.transform.SetParent(null);
         }
     }
+
+    private Vector3 DirectionVector(Vector3 pointA,  Vector3 pointB)
+    {
+        return (pointB - pointA).normalized;
+    }
+
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+        {
+            // Đổi hướng di chuyển khi đến điểm B
+            if (targetPosition == pointB.position)
+            {
+                targetPosition = pointA.position; // Chuyển sang điểm A
+            }
+            else
+            {
+                targetPosition = pointB.position; // Chuyển sang điểm B
+            }
+        }
+    }
+
+
+    IEnumerator MovePointToPoint()
+    {
+        while (isEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                // Đổi hướng di chuyển khi đến điểm B
+                if (targetPosition == pointB.position)
+                {
+                    targetPosition = pointA.position; // Chuyển sang điểm A
+                }
+                else
+                {
+                    targetPosition = pointB.position; // Chuyển sang điểm B
+                }
+                yield return new WaitForSeconds(1);
+            }
+            yield return null;
+        }
+
+    }    
 }
