51475fafa1a493b27aeeabfe1122d6b910741f07
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-06-10 01:52:20 +0700

    + Add script control character

---
+++ Assets/_Game/Scripts/EnemySight.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    public Enemy enemy;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            enemy.SetTarget(collision.GetComponent<Character>());
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            enemy.SetTarget(null);
+
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Kunai.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kunai : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        OnInit();
+    }
+
+    public void OnInit()
+    {
+        rb.velocity = transform.right * 5f;
+        Invoke(nameof(OnDespawn), 4);
+    }
+
+    public void OnDespawn()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Character>().OnHit(30);
+
+            OnDespawn();
+        }
+    }
+}
